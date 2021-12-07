d640ab7ccfc3097419ffb46c8a39c418a0e550f5
조우준 <jowoojun77@gmail.com>
2021-12-07 16:08:32 +0900

    Add: 누락된 파일 추가

--- pages/destination/[planet].js
+++ pages/destination/[planet].js
@@ -4,18 +4,18 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import PlanetTemplate from '../../components/templates/PlanetTemplate'
 
-import { LOAD_DESTINATION_REQUEST } from '../../reducers/destination';
+import { LOAD_DESTINATION_BY_NAME_REQUEST } from '../../reducers/destination';
 
 export default function Planet() {
   const router = useRouter();
   const { planet } = router.query
   const dispatch = useDispatch();
   const { destination } = useSelector((state) => state.destination);
-  
+
   useEffect(() => {
     dispatch({
-      type: LOAD_DESTINATION_REQUEST,
-      data: { planet: planet },
+      type: LOAD_DESTINATION_BY_NAME_REQUEST,
+      data: { planet: planet }
     })
   }, [planet])
 
